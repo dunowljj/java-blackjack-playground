887994a6f3d2cce3cedd2df9f4049a3142f1be0b
dunowljj <uuufg3322@gmail.com>
2022-07-17 15:19:14 +0900

    mrf 실습 2

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -28,6 +28,12 @@ public class StreamStudy {
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter((word) -> word.length() > 12)
+                .sorted((w1, w2) -> w2.length() - w1.length())
+                .distinct()
+                .limit(100)
+                .forEach((word) -> System.out.println(word.toLowerCase()));
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
